4710e752db63d0556c33f98ee9b7e655ae20fe4c
enormity001 <70377840+enormity001@users.noreply.github.com>
2021-02-28 20:15:21 +0800

    Update 积分.js

--- 积分.js
+++ 积分.js
@@ -1072,7 +1072,9 @@ function battleprc(
               sleep(3000); //间隔尽量大一点，容易卡住
               click(2043 + xoff, 68);
               sleep(3000); //
-              while (!sell()); ////
+              while (!sell()){
+                sleep(2000)
+              }; ////
               sellflag = true;
               break re;
             }
@@ -1268,7 +1270,8 @@ function sell(){
   sleep(2000);
 
   //进入卖装备界面
-  while (true) {
+  for(let counter = 0;;counter++){
+    if(counter > 20){return;}//sell查找次数
     var sellImg = images.read("./sell.jpg");
 
       
@@ -1289,6 +1292,7 @@ function sell(){
     } else {
       sleep(1500)
       click(sell.x, sell.y);
+      
         click(sell.x, sell.y);
       sell = null;
       log("open sell");
@@ -1298,10 +1302,9 @@ function sell(){
   }
   sleep(1500);
 
-  while (true) {
+  for(let counter = 0;;counter++) {
+    if(counter > 20){return;}//oma查找次数
     //找oma
-
-      
       var oma = FindMultiColors(
         
         "#ffffff",
@@ -1333,8 +1336,8 @@ function sell(){
         //   
         // }
         while(true){
-  
-              
+
+            
           var redbt = DetectsColor( "#ff5533", 1255+xoff, 772,threshold = 30)
             
           if(!redbt){
@@ -1380,7 +1383,8 @@ function sell(){
   }
 
   //sell主体部分（循环
-  oma: while (true) {
+  oma:  for(let counter = 0;;counter++) {
+    if(counter > 20){return;}//oma查找次数
     //先omakase售卖
 
       
@@ -1405,7 +1409,8 @@ function sell(){
       log("open oma");
       sleep(1000);
 
-      while (true) {
+      for(let counter = 0;;counter++) {
+        if(counter > 20){return;}//ensure查找次数
         //判断oma是否弹出界面
 
           
@@ -1447,7 +1452,7 @@ function sell(){
             //   }
             //   
             // }
-            while(true){
+            while(true){//3星
 
               
               var redbt1 = DetectsColor( "#ff5533", 1223+xoff, 382)
@@ -1457,7 +1462,7 @@ function sell(){
               sleep(200);
             }else{break;}
           }
-            while(true){
+            while(true){//4星
 
               
               var redbt2 = DetectsColor( "#ff5533", 1528+xoff, 376)
@@ -1474,7 +1479,8 @@ function sell(){
           click(1404+xoff, 900);
 
           //4星以下是否卖完
-          equiploop: while (true) {
+          equiploop:  for(let counter = 0;;counter++) {
+            if(counter > 25){return;}//查找次数
             //judge按钮
 
             //按钮是否为红色
@@ -1498,18 +1504,19 @@ function sell(){
               log("open onensure");
               //白色cancel按钮
               var starimg = images.fromBase64("iVBORw0KGgoAAAANSUhEUgAAABYAAAAXCAYAAAAP6L+eAAAABHNCSVQICAgIfAhkiAAAAiBJREFUOI21lLtvE0EQh787n2/x7RpjHEEkFJzg89kFDeFvoKaioE+FlAoJUaFUCAEVj7+BlNTQIdJSIYgUEkwRCcUPydi5PPw4irOT2Le+xEH8yp2Zb2dmZ8fIplXARIWmtGXyyJ3hwfxl3lYaPP9Ro9Xt61yPZOphwYinpwQlKTCAshR4jj3qpknNjPMYnnjSxlP24BKbkhKTi5yc8egVTsLEUzZzqSQAc6kkRWWTSmhDJ4PHq/KkTVmFbQDCdiiBJ+2zg3WvWFICT4rIWUmdEayDCtPAkzb5QRuGyqeSlJRAmIYmSpPxuLxBZuPxphE+YjGmHdbS9aw2W4CbacGtTEprW8xcYCmf5WvrQGs3gnu3Yz7I+RU/M/8C3vIP/ws48W2/t3JVWCw40Uc6j3oBfKi2STQCY+VT3We/H+ApG3nKj4pT9bDLm0qDpxtVjEvKCQCShsGdK4qHhRyLEyYhTl+ae7zcrPNxp00nCI7BQ5WV4HFxhruzF88Mff/7D882aqy3j0cvUncvCOhOOYDdIIw7qQjYlQL3lAUTiXFsCmMxEXBBJqcGF5UdiRkBzwoLV4qpJ0MmTFwpmBWWHuzKaEkQbr61hs+LzRprDV+7W1w5mrUVZwTwe33ebTd5tVXn116HeSfJ8kKO+9cyOCcqGyb1ueGPgpVlUhgrp+J3eP2zzup2k91e/+jsyfoO31sHLN/IHe3qsI3hB9vt9fkLhOecQDQZ96cAAAAASUVORK5CYII=")
-              while(true){
+              for(let counter = 0;;counter++) {
+                if(counter > 50){return;}//star查找次数
 
-               
-               var star = FindImageInRegion(starimg,966+xoff,529,30,30,0.83)
-             
-           if(star){
+              
+              var star = FindImageInRegion(starimg,966+xoff,529,30,30,0.83)
+            
+          if(star){
             sleep(200)
-             break;
-           }else{sleep(300)}
+            break;
+          }else{sleep(300)}
 
-           }
-           starimg = undefined;
+          }
+          starimg = undefined;
               press(1013+xoff, 648,1);
               press(1443+xoff, 762,1);
 
@@ -1532,9 +1539,9 @@ function sell(){
               log("found offensure");
               offensure = null;
               sleep(1300)
-         adsell: while (true) {
-               if(!box1Img){ var box1Img = images.read("./box1.jpg");}
-               if(!box2Img){ var box2Img = images.read("./box2.jpg");}
+        adsell: while (true) {
+              if(!box1Img){ var box1Img = images.read("./box1.jpg");}
+              if(!box2Img){ var box2Img = images.read("./box2.jpg");}
 
                 
                   var mbox1 = MatchTemplate( box1Img, {
@@ -1573,7 +1580,8 @@ function sell(){
                   sleep(100);
                   //红五角星
                   var starimg = images.fromBase64("iVBORw0KGgoAAAANSUhEUgAAABYAAAAXCAYAAAAP6L+eAAAABHNCSVQICAgIfAhkiAAAAiBJREFUOI21lLtvE0EQh787n2/x7RpjHEEkFJzg89kFDeFvoKaioE+FlAoJUaFUCAEVj7+BlNTQIdJSIYgUEkwRCcUPydi5PPw4irOT2Le+xEH8yp2Zb2dmZ8fIplXARIWmtGXyyJ3hwfxl3lYaPP9Ro9Xt61yPZOphwYinpwQlKTCAshR4jj3qpknNjPMYnnjSxlP24BKbkhKTi5yc8egVTsLEUzZzqSQAc6kkRWWTSmhDJ4PHq/KkTVmFbQDCdiiBJ+2zg3WvWFICT4rIWUmdEayDCtPAkzb5QRuGyqeSlJRAmIYmSpPxuLxBZuPxphE+YjGmHdbS9aw2W4CbacGtTEprW8xcYCmf5WvrQGs3gnu3Yz7I+RU/M/8C3vIP/ws48W2/t3JVWCw40Uc6j3oBfKi2STQCY+VT3We/H+ApG3nKj4pT9bDLm0qDpxtVjEvKCQCShsGdK4qHhRyLEyYhTl+ae7zcrPNxp00nCI7BQ5WV4HFxhruzF88Mff/7D882aqy3j0cvUncvCOhOOYDdIIw7qQjYlQL3lAUTiXFsCmMxEXBBJqcGF5UdiRkBzwoLV4qpJ0MmTFwpmBWWHuzKaEkQbr61hs+LzRprDV+7W1w5mrUVZwTwe33ebTd5tVXn116HeSfJ8kKO+9cyOCcqGyb1ueGPgpVlUhgrp+J3eP2zzup2k91e/+jsyfoO31sHLN/IHe3qsI3hB9vt9fkLhOecQDQZ96cAAAAASUVORK5CYII=")
-                 while(true){
+                  for(let counter = 0;;counter++) {
+                    if(counter > 50){return;}//star查找次数
 
                   
                   var star = FindImageInRegion(starimg,966+xoff,529,30,30,0.83)
@@ -1594,10 +1602,9 @@ function sell(){
                   mbox2 = [];
                   //判断sell完成
 
-            
-                  while(true){
+                  for(let counter = 0;;counter++) {
+                    if(counter > 50){return;}//star查找次数
 
-                      
                       var gbt = DetectsColor( "#888888", 1772+xoff, 1011)
                       
                     if(!gbt){
@@ -1676,8 +1683,6 @@ function sell(){
 
     //收取操作
     re: for (var i = 0; i < 20000; i++) {
-      //收取至少3次再判断背包是否满了
-     
 
           
           var nospace = FindMultiColors("#ffffff",[[-646,89,"#88dddd"],[-647,-481,"#88dddd"],[745,-480,"#88dddd"],[743,91,"#88dddd"]],{region:[1150+xoff,708,43,41]})
